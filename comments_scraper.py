c021a61e6e99ddfbfc286b151015579cfdc9e42e
Benjamin Davis <ben.yj.davis@gmail.com>
2022-05-13 14:24:16 -0600

    First working draft

--- comments_scraper.py
+++ comments_scraper.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from KEYS import API_Key
 from googleapiclient.discovery import build
+from pprint import pprint
 
 service = build('youtube','v3',developerKey=API_Key)
 
 video_data = service.commentThreads().list(
-    part='snippet,replies',
-    id='uxPdPpi5W4o',
-    # textFormat='plainText'
+    videoId='uxPdPpi5W4o',
+    part='snippet',
+    maxResults=1
 ).execute()
-# print(video_data)
+
 for item in video_data['items']:
-    print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+    pprint(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+
